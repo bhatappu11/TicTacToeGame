6263e40211e89d2e4c7a8bda02c6cdd0e93e68e6
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-06 20:04:35 +0530

    ADDED: makemove method to make a move by player

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -3,39 +3,51 @@ package com.bridgelabz.tictactoe;
 import java.util.*;
 
 public class TicTacToeGame {
-	public static char[] gameBoard = new char[10];
+	public char[] gameBoard = new char[10];
+	Scanner scanner=new Scanner(System.in);
 	
-	public static char[] createBoard() {
+	public char[] createBoard() {
 		for(int index=1;index<gameBoard.length;index++) {
 			gameBoard[index]=' ';
 		}
 		return gameBoard;
 	}
-	public static char letterToPlay() {
+	public char letterToPlay() {
 		System.out.println("choose the letter to play option 1: X option 2: O");
-		Scanner scanner=new Scanner(System.in);
 		char letter=scanner.next().charAt(0);
 		return letter;
 	}
-	private static void displayBoard() {
+	private void displayBoard() {
 		for(int index=1;index<gameBoard.length;index++) {
 			if(index==4 || index==7)
 				System.out.println();
 			System.out.print(gameBoard[index]+"|");
 		}
+		System.out.println();
+	}
+	public void makeMove(char letter) {
+		System.out.println("Enter the index between 1-9");
+		int index=scanner.nextInt();
+		if(gameBoard[index]==' ') {
+			gameBoard[index]=letter;			
+		}
+		else
+			System.err.println("Index is not free");
+		
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe game");
-		char[] board = createBoard();
+		TicTacToeGame gameObject=new TicTacToeGame();
+		char[] board = gameObject.createBoard();
 		char computerLetter = ' ';
-		char playerLetter = letterToPlay();
-		if(playerLetter == 'X')
-			computerLetter = 'O';
-		else
-			computerLetter = 'X';
-		displayBoard();
+		char playerLetter = gameObject.letterToPlay();
+		computerLetter = playerLetter=='X'?'O':'X';
+		gameObject.displayBoard();
+		gameObject.makeMove(playerLetter);
+		gameObject.displayBoard();
 		
 	}
+	
 	
 
 }

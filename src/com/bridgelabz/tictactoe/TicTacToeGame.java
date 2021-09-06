ecd873f2da466d1b7731e4c39881a985596fdf4a
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-06 08:24:58 +0530

    ADDED: created gameBoard array to store board values

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,10 +1,13 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
+	public static char[] gameBoard = new char[10];
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe game");
-
+		for(int index=1;index<=10;index++) {
+			gameBoard[index]=' ';
+		}
 	}
 
 }

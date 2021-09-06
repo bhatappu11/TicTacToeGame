4bf7c2291f3d5ceb89dcaca512bcda0a4eb4ed5e
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-06 08:35:04 +0530

    REFACTOR: createBoard() method to initialise gameBoard array

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -2,12 +2,15 @@ package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
 	public static char[] gameBoard = new char[10];
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe game");
+	public static void createBoard() {
 		for(int index=1;index<=10;index++) {
 			gameBoard[index]=' ';
 		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Tic Tac Toe game");
+		createBoard();
 	}
 
 }

b8871db1b6b9d7ec043313612702b78f5a296f50
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-06 09:08:14 +0530

    ADDED: letterToPlay to choose player letter for the game

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,16 +1,33 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.*;
+
 public class TicTacToeGame {
 	public static char[] gameBoard = new char[10];
-	public static void createBoard() {
-		for(int index=1;index<=10;index++) {
+	
+	public static char[] createBoard() {
+		for(int index=1;index<gameBoard.length;index++) {
 			gameBoard[index]=' ';
 		}
+		return gameBoard;
+	}
+	public static char letterToPlay() {
+		System.out.println("choose the letter to play option 1: X option 2: O");
+		Scanner scanner=new Scanner(System.in);
+		char letter=scanner.next().charAt(0);
+		return letter;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe game");
-		createBoard();
+		char[] board = createBoard();
+		char computerLetter = ' ';
+		char playerLetter = letterToPlay();
+		if(playerLetter == 'X')
+			computerLetter = 'O';
+		else
+			computerLetter = 'X';
+		
 	}
 
 }
